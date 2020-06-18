0a9649a00369bb8f69984bfae219a519b2e64638
Pol Ribas <polribas14@gmail.com>
2020-06-18 17:07:45 +0200

    some minor fixes

--- js/la-linea-infinite-loading.js
+++ js/la-linea-infinite-loading.js
@@ -1,8 +1,12 @@
+
 jQuery(function($) {
-    $("#story").append('<span class="load-more">Cargar más</span>');
+    $("#story").append('<span class="load-more"></span>');
     var button = $('#story .load-more');
-    var page = 2;
+    var page = 1;
     var loading = false;
+
+    loadPosts();
+    
     var scrollHandling = {
         allow: true,
         reallow: function() {
@@ -17,24 +21,30 @@ jQuery(function($) {
             setTimeout(scrollHandling.reallow, scrollHandling.delay);
             var offset = $(button).offset().top - $(window).scrollTop();
             if(1000 > offset) {
-                loading = true;
-                var data = {
-                    action: 'la_linea_ajax_load_more', page: page
-                };
-                $.post(vars.url, data, function(res) {
-                    if(res.success) {
-                        res.data.forEach(function(post) {
-                            $('#story').append('<p>' + post.post_title + ' ' + post.post_date + '</p>');
-                        });
-                        $('#story').append(button);
-                        page = page + 1;
-                        loading = false;
-                    }
-                    else {
-
-                    }
-                });
+                loadPosts();
             }
         }
     });
+
+    function loadPosts() {
+        button.text("Cargando...");
+        loading = true;
+        var data = {
+            action: 'la_linea_ajax_load_more', page: page
+        };
+        $.post(vars.url, data, function(res) {
+            if(res.success) {
+                res.data.forEach(function(post) {
+                    $('#story').append('<p>' + post.post_title + ' ' + post.post_date + '</p>');
+                });
+                $('#story').append(button);
+                page = page + 1;
+                loading = false;
+                button.text("");
+            }
+            else {
+                button.text("");
+            }
+        });
+    }
 });
--- page-la-linea.php
+++ page-la-linea.php
@@ -11,31 +11,16 @@ require_once("functions/la_linea_functions.php");
 <?php
     if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
-    <div class="blog-post single">
+
+<div class="blog-post single la-linea">
+    
 	<h1 class="post-title"><?php the_title(); ?></h1>
 
     <?php get_template_part("template-parts/content", "post"); ?>
 
-    <div id="story">
-        <?php
-            $loop = new WP_Query( array( 'post_type' => ['post','books','revision'], "orderby" => "post_date", "order" => "DESC", "posts_per_page" => 10, 'post_status' => array('publish','inherit')) );
-            if ( $loop->have_posts() ) :
-                while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                    <p><?php echo get_the_title() . ' - ' . get_the_date() . ' - ' . get_post_type(); ?></p>
-                    <?php 
-                        $parent_id = wp_is_post_revision(get_the_ID());
-                        if($loop->post->post_type==='books' || ($parent_id!==false && get_post_type($parent_id)==='books')) {
-                            //var_dump(get_post_meta( get_the_ID(), 'idioma', 'single' ));
-                        }
-                    ?>  
-                <?php endwhile;
-            endif;
-            wp_reset_postdata();
+    <div id="story"></div>
 
-            //preparem el loading infinit
-        ?>
-    </div>
-</div><!-- /.blog-page -->
+</div><!-- /.blog-post -->
 
 <?php        
     endwhile; endif;
